5d90affd14353a5b2d73d093d2e3e370bdab095b
flaviaglenda <flaviaglenda15@gmail.com>
2024-09-25 19:21:28 -0300

    exercicio 3 completo

--- exercicio3/processar_reserva.php
+++ exercicio3/processar_reserva.php
@@ -12,10 +12,11 @@ $sala = $_POST['select'];
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script> 
     <link rel="stylesheet" href="../exercicio3/reserva.css">
+    <link href="https://fonts.googleapis.com/css?family=Poppins:200,300,400,500,600,700&display=swap" rel="stylesheet">
     <title>Reserva Confirmada</title>
 </head>
 <body>
-<nav class="navbar"></nav>
+<nav class="navbar">
         <ul>
             <li><a href="../exercicio1/cadastrar.php">Home</a></li>
             <li><a href="../exercicio1/form.php">Exercício 1</a></li>
@@ -23,6 +24,33 @@ $sala = $_POST['select'];
             <li><a href="../exercicio3/reserva.php">Exercício 3</a></li>
         </ul>
     </nav>
+
+    <div class="area">
+        <ul class="circles">
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+</ul>
+            <div class="confirmation-message">
+        <h1>Sua sala foi reservada!</h1>
+        <h3>Aqui estão as informações: </h3>
+        <div class="reservation-info">
+                <p><strong>Nome:</strong> <?php echo $nome; ?></p>
+                <p><strong>Data:</strong> <?php echo $data; ?></p>
+                <p><strong>Hora:</strong> <?php echo $hora; ?></p>
+                <p><strong>Sala:</strong> <?php echo $sala; ?></p>
+            </div>
+            <a href="../exercicio1/form.php" class="button">Voltar</a>
+    </div>
+</div>
+</div>
     <script>
         const nome = "<?php echo $nome; ?>";
         const data = "<?php echo $data; ?>";
