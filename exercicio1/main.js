55b6fd71e8dd465c5bf093adcb7d6389ee0c6989
lucasbalderrama <abreulucas3715@gmail.com>
2024-09-26 03:05:09 -0300

    js ex1

--- exercicio1/main.js
+++ exercicio1/main.js
@@ -1,5 +1,28 @@
-Swal.fire({
-    title: "Cliente cadastrado",
-    text: "Cliente cadastrado com sucesso",
-    icon: "success"
-  });
+// Flávia, Lanna e Lucas
+
+document.addEventListener('DOMContentLoaded', function() {
+    const formulario = document.getElementById('formulario');
+
+    formulario.addEventListener('submit', function(event) {
+        event.preventDefault();
+
+        if (formulario.method === 'POST' || 'post') {
+            Swal.fire({
+                title: "Cliente cadastrado com sucesso",
+                icon: "success"
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    formulario.submit(); 
+                }
+            });
+        } else {
+            Swal.fire({
+                icon: "error",
+                title: "Oops...",
+                text: "O formulário não foi enviado corretamente",
+            }).then(() => {
+                formulario.reset();
+            });
+        }
+    });
+});
